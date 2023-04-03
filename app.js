6c67075fa4118e16a5970d599d64d12177b0f08e
demotivate <50751626+demotivate@users.noreply.github.com>
2023-04-02 21:30:44 -0600

    294. Challenge 10

--- app.js
+++ app.js
@@ -28,6 +28,18 @@ app.get("/contact", (req, res) => {
     res.render("contact", {content: contactContent});
 });
 
+app.get("/compose", (req, res) => {
+    res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.title,
+        content: req.body.postContent,
+    }
+    console.log(post);
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
